3e3a367462eb54a3d546672931bac3343b126d58
Ivan Smetanin <smetanin23@yandex.ru>
2017-10-21 13:32:06 +0300

    Add select favors layout

--- InSearch/InSearch/Screens/SelectFavors/View/SelectFavorsViewController.swift
+++ InSearch/InSearch/Screens/SelectFavors/View/SelectFavorsViewController.swift
@@ -21,81 +21,37 @@ final class SelectFavorsViewController: UIViewController {
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
+    var adapter: SelectFavorsTableViewAdapter?
 
     // MARK: - IBActions
 
     @IBAction private func doneButtonAction(_ sender: UIButton) {
-
+        
     }
 
     // MARK: - UIViewController
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        edgesForExtendedLayout = []
-        DispatchQueue.main.sync {
-            if #available(iOS 10.0, *) {
-                UIApplication.shared.open(URL(string: "google.com")!)
-            } else {
-                UIApplication.shared.openURL(URL(string: "google.com")!)
-            }
-        }
+        setupInitialState()
     }
 
-    // MARK: - Internal helpers
+    // MARK: - Private helpers
 
-}
-
-import UIKit
-
-protocol SelectFavorsViewAdapterOutput {
-
-}
-
-final class SelectFavorsTableViewAdapter: NSObject {
-
-    // MARK: - Constants
-
-//    fileprivate let output: SelectFavorsViewAdapterOutput
-
-    // MARK: - Properties
-
-    fileprivate var items: [String]
-    fileprivate (set) var tableView: UITableView {
-        didSet {
-            tableView.register(UINib(nibName: "", bundle: nil), forCellReuseIdentifier: "")
-        }
+    private func configureView(with categories: [Category]) {
+        let adapter = SelectFavorsTableViewAdapter(items: categories, tableView: tableView)
+        tableView.delegate = adapter
+        tableView.dataSource = adapter
+        self.adapter = adapter
     }
 
-    // MARK: - Initialization and deinitialization
-
-    init(items: [String], tableView: UITableView) {
-        self.items = items
-        self.tableView = tableView
+    private func setupInitialState() {
+        tableView.estimatedRowHeight = 66
+        tableView.rowHeight = UITableViewAutomaticDimension
     }
 
-}
-
-
-// MARK: - UITableViewDataSource
-
-extension SelectFavorsTableViewAdapter: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        return cell
-    }
-}
-
-
-// MARK: - UITableViewDelegate
-
-extension SelectFavorsTableViewAdapter: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        items[0] = "kek"
+    private func fillStaticTexts() {
+        titleLabel.text = L10n.Favors.title
+        doneButton.setTitle(L10n.Favors.done, for: .normal)
     }
 }
---
+++ InSearch/InSearch/Services/Categories.swift
@@ -0,0 +1,9 @@
+//
+//  Categories.swift
+//  InSearch
+//
+//  Created by Ivan Smetanin on 21/10/2017.
+//  Copyright © 2017 9th Bit. All rights reserved.
+//
+
+import Foundation
