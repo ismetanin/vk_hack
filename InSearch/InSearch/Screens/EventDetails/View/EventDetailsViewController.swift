d5b8ede80a97737a683325a1c768e8d15ac544ef
Gregory Berngardt <gregoryvit@gmail.com>
2017-10-21 22:49:45 +0300

    Added EventDetailsViewController

---
+++ InSearch/InSearch/Screens/EventDetails/View/EventDetailsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EventDetailsViewController.swift
+//  InSearch
+//
+//  Created by Gregory Berngardt on 21/10/2017.
+//  Copyright © 2017 9th Bit. All rights reserved.
+//
+
+import UIKit
+
+class EventDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
