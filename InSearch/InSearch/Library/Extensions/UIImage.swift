dc2f58c2f89a1e6250bb98017b183cdad4ae644b
Gregory Berngardt <gregoryvit@gmail.com>
2017-10-21 18:47:51 +0300

    Added initial version of EventsViewController

---
+++ InSearch/InSearch/Library/Extensions/UIImage.swift
@@ -0,0 +1,27 @@
+//
+//  UIImage.swift
+//  InSearch
+//
+//  Created by Gregory Berngardt on 21/10/2017.
+//  Copyright © 2017 9th Bit. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    public func image(withTintColor color: UIColor) -> UIImage{
+        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
+        let context: CGContext = UIGraphicsGetCurrentContext()!
+        context.translateBy(x: 0, y: self.size.height)
+        context.scaleBy(x: 1.0, y: -1.0)
+        context.setBlendMode(CGBlendMode.normal)
+        let rect: CGRect = CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height)
+        context.clip(to: rect, mask: self.cgImage!)
+        color.setFill()
+        context.fill(rect);
+        let newImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!;
+        UIGraphicsEndImageContext();
+        return newImage;
+        
+    }
+}
---
+++ InSearch/InSearch/Screens/Events/View/Cells/EventCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  EventCollectionViewCell.swift
+//  InSearch
+//
+//  Created by Gregory Berngardt on 21/10/2017.
+//  Copyright © 2017 9th Bit. All rights reserved.
+//
+
+import UIKit
+
+class EventCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
